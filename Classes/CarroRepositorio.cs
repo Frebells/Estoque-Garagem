7e436257dc1f523ca48b28fb1a4970c891bc925f
Frebells <leo14bellei@gmail.com>
2021-02-25 22:58:43 -0400

    Rodando primeiras opcoes para tst

--- Classes/CarroRepositorio.cs
+++ Classes/CarroRepositorio.cs
@@ -2,7 +2,34 @@ using System;
 using System.Collections.Generic;
 using CadastroEstoque.Interfaces;
 
-namespace CadastroEstoque.Interfaces
+namespace CadastroEstoque
 {
     public class CarroRepositorio : IRepositorio<CRUDItens>
     {
+        private List<CRUDItens> listacarro = new List<CRUDItens>();
+        public void Atualiza(int id, CRUDItens objeto)
+        {
+            listacarro[id] = objeto;
+        }
+        public void Excluir(int id)
+        {
+            listacarro[id].SemEstoque();
+        }
+        public void Insere(CRUDItens objeto)
+        {
+            listacarro.Add(objeto);
+        }
+        public List<CRUDItens> Lista()
+        {
+            return listacarro;
+        }
+        public int ProximoId()
+        {
+            return listacarro.Count;
+        }
+        public CRUDItens RetornaId(int id)
+        {
+            return listacarro[id];
+        }
+    }
+}
